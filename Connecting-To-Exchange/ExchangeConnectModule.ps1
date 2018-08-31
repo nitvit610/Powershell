c659c1b6096739d309f7b4d135931d509818b85c
nitvit610 <nitvit610@gmail.com>
2018-08-31 14:40:45 +0100

    Add functions to script and output to test

--- Connecting-To-Exchange/ExchangeConnectModule.ps1
+++ Connecting-To-Exchange/ExchangeConnectModule.ps1
@@ -2,6 +2,16 @@
 
 . C:\Users\woodso\Documents\GitHub\Powershell\Framework\ConnectToExchangeServer.ps1
 
+Write-Host "Scripts have loaded"
+
+ElevateSession
+
+Write-Host "Session has been elevated"
+
+ConnectExchange
+
+Write-Host "Connected to exchange"
+
 Get-Mailbox -Identity "Oscar Woods"
 
 Remove-PSSession
