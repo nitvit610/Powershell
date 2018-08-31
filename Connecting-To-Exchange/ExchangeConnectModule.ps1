15af0ea630d7d474d8aa345189c62a785658de9f
nitvit610 <nitvit610@gmail.com>
2018-08-31 14:28:30 +0100

    Add links to framework scripts

---
+++ Connecting-To-Exchange/ExchangeConnectModule.ps1
@@ -0,0 +1,7 @@
+. C:\Users\woodso\Documents\GitHub\Powershell\Framework\ElevateAdmin.ps1
+
+. C:\Users\woodso\Documents\GitHub\Powershell\Framework\ConnectToExchangeServer.ps1
+
+Get-Mailbox -Identity "Oscar Woods"
+
+Remove-PSSession
