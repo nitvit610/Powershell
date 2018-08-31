41d23419e5d5bf459fee42460cdf3cb259c3c424
nitvit610 <nitvit610@gmail.com>
2018-08-31 12:40:48 +0100

    Add inputs for user mailbox and do while loops for sign in and error handling

--- Connecting-To-Exchange/ExchangeConnect.ps1
+++ Connecting-To-Exchange/ExchangeConnect.ps1
@@ -9,12 +9,52 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 
 Set-ExecutionPolicy Unrestricted
 
-$UserCredential = Get-Credential
+Do {
+    Try {
+        $Failed = $false
+        $ErrorActionPreference = "Stop"
 
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://rspex01.willmottresidential.co.uk/PowerShell/ -Authentication Kerberos -Credential $UserCredential
+        $UserCredential = Get-Credential
 
-Import-PSSession $Session -DisableNameChecking
+        $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://rspex01.willmottresidential.co.uk/PowerShell/ -Authentication Kerberos -Credential $UserCredential
 
-Enable-Mailbox -Identity "Test McTest"
+        Import-PSSession $Session -DisableNameChecking
+
+    } Catch {
+        Write-Host "`nLogin attempt failed, please try again`n"
+        $Failed = $true
+
+    } Finally {
+        $ErrorActionPreference = "Continue"
+
+    }
+
+} While ($Failed -eq $true)
+        
+clear
+
+Do {
+    $Failed = $false
+
+    $NewUserInput = Read-Host -Prompt "Please input the new users name (e.g. Oscar Woods)"
+
+    Try {
+        Write-Host $NewUserInput
+        $ErrorActionPreference = "Stop"
+        Enable-Mailbox -Identity $NewUserInput
+
+        Write-Host "The user's mailbox has been successfully created"
+
+    } Catch {
+        Write-Host "`nSorry that user doesn't exist or already has a mailbox, please try again"
+
+        $Failed = $true
+
+    } Finally {
+        $ErrorActionPreference = "Continue"
+
+    }
+
+} While ($Failed -eq $true)
 
 Remove-PSSession
