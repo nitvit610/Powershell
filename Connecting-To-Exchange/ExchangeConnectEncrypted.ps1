84e20016a572e1458d5cb8ad09d92fb46b5af3f2
nitvit610 <nitvit610@gmail.com>
2018-08-31 16:17:37 +0100

    Edit SecurePass to make script work

--- Connecting-To-Exchange/ExchangeConnectEncrypted.ps1
+++ Connecting-To-Exchange/ExchangeConnectEncrypted.ps1
@@ -1,14 +1,12 @@
 $AdminName = "RESIDENTIAL\woodso"
-
 $Pass = Get-Content "C:\Users\woodso\Documents\cred.txt"
-$SecurePass = ConvertTo-SecureString -String $Pass -AsPlainText -Force
-
+$SecurePass = $Pass | ConvertTo-SecureString
 $Cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $AdminName, $SecurePass
 
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://rspex01.willmottresidential.co.uk/powershell/ -Credential $Cred -AllowRedirection
 
 Import-PSSession $Session -DisableNameChecking
 
-Get-Mailbox
+Get-Mailbox -Identity "Oscar Woods"
 
 Remove-PSSession $Session
